235ce60709fda598a85e72fddab074c78d47bdc5
lennox1 <lennox1@gmail.com>
2026-07-20 18:43:38 -0600

    Se creo el class Cronometro

---
+++ src/cronometros/Cronometro.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cronometros;
+
+import javax.swing.Timer;
+
+/**
+ *
+ * @author lenno
+ */
+public class Cronometro {
+
+    private int segundos;
+    private final Timer timer;
+
+    public Cronometro() {
+        segundos = 0;
+
+        timer = new Timer(1000, evento -> Aumentar_tiempo());
+    }
+
+    private void Aumentar_tiempo() {
+        segundos++;
+    }
+
+    public void iniciar() {
+        timer.start();
+    }
+
+    public void detener() {
+        if (timer != null) {
+            timer.stop();
+        }
+    }
+
+    public void reiniciar() {
+        detener();
+        segundos = 0;
+    }
+
+    public int getTiempo() {
+        return segundos;
+    }
+
+    public String getTiempo_reiniciado() {
+        int minutos = segundos / 60;
+        int seg = segundos % 60;
+
+        if (seg < 10) {
+            return minutos + ":0" + seg;
+        } else {
+            return minutos + ":" + seg;
+        }
+    }
+}
--- src/niveles/Nivel.java
+++ src/niveles/Nivel.java
@@ -8,6 +8,23 @@ package niveles;
  *
  * @author lenno
  */
-public class Nivel {
-    
+public enum Nivel {
+
+    PRINCIPIANTE(8),
+    INTERMEDIO(16),
+    AVANZADO(32);
+
+    private final int parejas;
+
+    Nivel(int parejas) {
+        this.parejas = parejas;
+    }
+
+    public int getParejas() {
+        return parejas;
+    }
+
+    public int getTotalCartas() {
+        return parejas * 2;
+    }
 }
