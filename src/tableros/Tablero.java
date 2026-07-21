538e5bb7239cd5fbda09fd1ef4ec4f42951dca4c
Bryan <bryanlaraarguello@gmail.com>
2026-07-20 18:41:15 -0600

    Creacion de tablero
    Creacion de sus metodos

--- src/tableros/Tablero.java
+++ src/tableros/Tablero.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tableros;
 
-/**
- *
- * @author bryan
- */
+package tableros;
+import cartas.Carta;
+
 public class Tablero {
+    private Carta[][] Tablero;
+    private int filas;
+    private int columnas;
+    
+    
     
 }
